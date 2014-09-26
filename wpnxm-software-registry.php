2eb4d6234286989bda3bffdfa77ee440526a4faa
jakoch <jakoch@web.de>
2014-09-27 01:52:46 +0200

    updated software registry - PHP x64 v5.6.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Fri, 26 Sep 2014 23:52:38 +0000.
+    * Last Update Fri, 26 Sep 2014 23:52:44 +0000.
     * Do not edit manually!
     */
 
@@ -703,9 +703,10 @@
     '5.5.16' => 'http://windows.php.net/downloads/releases/archives/php-5.5.16-nts-Win32-VC11-x64.zip',
     '5.5.17' => 'http://windows.php.net/downloads/releases/php-5.5.17-nts-Win32-VC11-x64.zip',
     '5.6.0' => 'http://windows.php.net/downloads/releases/php-5.6.0-nts-Win32-VC11-x64.zip',
+    '5.6.1' => 'http://windows.php.net/downloads/releases/php-5.6.1-nts-Win32-VC11-x64.zip',
     'latest' => array(
-      'version' => '5.6.0',
-      'url' => 'http://windows.php.net/downloads/releases/php-5.6.0-nts-Win32-VC11-x64.zip',
+      'version' => '5.6.1',
+      'url' => 'http://windows.php.net/downloads/releases/php-5.6.1-nts-Win32-VC11-x64.zip',
     ),
   ),
   'phpext_amqp' => array(
