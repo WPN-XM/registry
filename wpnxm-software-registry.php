53c9a11ac68dab046960a9b0c2948b9e52578325
jakoch <jakoch@web.de>
2014-10-03 18:20:08 +0200

    added missing comma

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -55,7 +55,7 @@
   ),
   'gogs' => array(
     'name' = 'Gogs (Go Git Service)',
-    'website' => 'http://gogs.io/'
+    'website' => 'http://gogs.io/',
     '0.5.2' => 'https://github.com/gogits/gogs/releases/download/v0.5.2/windows_amd64.zip',
     'latest' => array(
       'version' => '0.5.2',
