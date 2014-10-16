857053abf951dc78817994d1d7ec929cfc0cda5c
jakoch <jakoch@web.de>
2014-10-16 02:55:28 +0200

    updated software registry - phpMyAdmin v4.2.9.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Thu, 16 Oct 2014 00:55:21 +0000.
+    * Last Update Thu, 16 Oct 2014 00:55:26 +0000.
     * Do not edit manually!
     */
 
