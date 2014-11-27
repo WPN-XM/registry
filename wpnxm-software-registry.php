680f309c9e7b92a62c975de8cfd77d39aabb9369
jakoch <jakoch@web.de>
2014-11-27 18:07:30 +0100

    updated software registry - Imagick v6.8.9-10

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Thu, 27 Nov 2014 17:07:20 +0000.
+    * Last Update Thu, 27 Nov 2014 17:07:28 +0000.
     * Do not edit manually!
     */
 
