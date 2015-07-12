cbac82abe94ed64e6b951fbce1603bde31d9a495
jakoch <jakoch@web.de>
2015-07-12 11:07:19 +0200

    updated software registry - NodeJS x32 v0.12.7

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Sun, 12 Jul 2015 09:06:44 +0000.
+    * Last Update Sun, 12 Jul 2015 09:07:05 +0000.
     * Do not edit manually!
     */
 
@@ -557,9 +557,11 @@
     '0.12.3' => 'http://nodejs.org/dist/v0.12.3/node.exe',
     '0.12.4' => 'http://nodejs.org/dist/v0.12.4/node.exe',
     '0.12.5' => 'http://nodejs.org/dist/v0.12.5/node.exe',
+    '0.12.6' => 'http://nodejs.org/dist/v0.12.6/node.exe',
+    '0.12.7' => 'http://nodejs.org/dist/v0.12.7/node.exe',
     'latest' => array(
-      'version' => '0.12.5',
-      'url' => 'http://nodejs.org/dist/v0.12.5/node.exe',
+      'version' => '0.12.7',
+      'url' => 'http://nodejs.org/dist/v0.12.7/node.exe',
     ),
   ),
   'node-x64' => array(
