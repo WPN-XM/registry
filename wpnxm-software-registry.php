3ebcfaf861ed823616b5bbf3d8440808e58fae7a
jakoch <jakoch@web.de>
2015-07-31 19:54:38 +0200

    updated software registry - MongoDB v3.1.5

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Fri, 31 Jul 2015 17:54:09 +0000.
+    * Last Update Fri, 31 Jul 2015 17:54:29 +0000.
     * Do not edit manually!
     */
 
