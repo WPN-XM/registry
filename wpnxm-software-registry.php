1dcbe77579f76e24038c249a7e2e31343b4f654d
jakoch <jakoch@web.de>
2015-07-31 19:55:50 +0200

    updated software registry - PHP QA x64 v7.0.0beta2

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Fri, 31 Jul 2015 17:55:26 +0000.
+    * Last Update Fri, 31 Jul 2015 17:55:42 +0000.
     * Do not edit manually!
     */
 
@@ -1023,9 +1023,10 @@
     '7.0.0alpha1' => 'http://windows.php.net/downloads/qa/php-7.0.0alpha1-nts-Win32-VC14-x64.zip',
     '7.0.0alpha2' => 'http://windows.php.net/downloads/qa/php-7.0.0alpha2-nts-Win32-VC14-x64.zip',
     '7.0.0beta1' => 'http://windows.php.net/downloads/qa/php-7.0.0beta1-nts-Win32-VC14-x64.zip',
+    '7.0.0beta2' => 'http://windows.php.net/downloads/qa/php-7.0.0beta2-nts-Win32-VC14-x64.zip',
     'latest' => array(
-      'version' => '7.0.0beta1',
-      'url' => 'http://windows.php.net/downloads/qa/php-7.0.0beta1-nts-Win32-VC14-x64.zip',
+      'version' => '7.0.0beta2',
+      'url' => 'http://windows.php.net/downloads/qa/php-7.0.0beta2-nts-Win32-VC14-x64.zip',
     ),
   ),
   'php-x64' => array(
