771e2155bfa4ae65926ec5a795e3753868944305
jakoch <jakoch@web.de>
2015-08-21 12:05:01 +0200

    updated software registry - PHP QA x86 v7.0.0RC1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Thu, 20 Aug 2015 16:07:57 +0000.
+    * Last Update Fri, 21 Aug 2015 10:04:47 +0000.
     * Do not edit manually!
     */
 
@@ -1017,14 +1017,15 @@
   'php-qa' => array(
     'name' => 'PHP QA x86',
     'website' => 'http://php.net/',
-    '5.6.11RC1' => 'http://windows.php.net/downloads/qa/php-5.6.11RC1-nts-Win32-VC11-x86.zip',
-    '7.0.0alpha2' => 'http://windows.php.net/downloads/qa/php-7.0.0alpha2-nts-Win32-VC14-x86.zip',
-    '7.0.0beta1' => 'http://windows.php.net/downloads/qa/php-7.0.0beta1-nts-Win32-VC14-x86.zip',
-    '7.0.0beta2' => 'http://windows.php.net/downloads/qa/php-7.0.0beta2-nts-Win32-VC14-x86.zip',
-    '7.0.0beta3' => 'http://windows.php.net/downloads/qa/php-7.0.0beta3-nts-Win32-VC14-x86.zip',
+    '5.6.11RC1' => 'http://windows.php.net/downloads/qa/archives/php-5.6.11RC1-nts-Win32-VC11-x86.zip',
+    '7.0.0alpha2' => 'http://windows.php.net/downloads/qa/archives/php-7.0.0alpha2-nts-Win32-VC14-x86.zip',
+    '7.0.0beta1' => 'http://windows.php.net/downloads/qa/archives/php-7.0.0beta1-nts-Win32-VC14-x86.zip',
+    '7.0.0beta2' => 'http://windows.php.net/downloads/qa/archives/php-7.0.0beta2-nts-Win32-VC14-x86.zip',
+    '7.0.0beta3' => 'http://windows.php.net/downloads/qa/archives/php-7.0.0beta3-nts-Win32-VC14-x86.zip',
+    '7.0.0RC1' => 'http://windows.php.net/downloads/qa/php-7.0.0RC1-nts-Win32-VC14-x86.zip',
     'latest' => array(
-      'version' => '7.0.0beta3',
-      'url' => 'http://windows.php.net/downloads/qa/php-7.0.0beta3-nts-Win32-VC14-x86.zip',
+      'version' => '7.0.0RC1',
+      'url' => 'http://windows.php.net/downloads/qa/php-7.0.0RC1-nts-Win32-VC14-x86.zip',
     ),
   ),
   'php-qa-x64' => array(
