c693f6e398495c992aa1679b0bec0311297c2b29
jakoch <jakoch@web.de>
2015-09-10 12:05:41 +0200

    updated software registry - NodeJS x32 v4.0.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Thu, 10 Sep 2015 10:02:37 +0000.
+    * Last Update Thu, 10 Sep 2015 10:05:39 +0000.
     * Do not edit manually!
     */
 
