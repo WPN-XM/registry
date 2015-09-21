6f3188c437d33e7ef0f7deb9416ceec853ef34d4
jakoch <jakoch@web.de>
2015-09-21 02:30:16 +0200

    fix latest version of phpmyadmin v4.4.15

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -2389,10 +2389,10 @@
     '4.4.11' => 'https://files.phpmyadmin.net/phpMyAdmin/4.4.11/phpMyAdmin-4.4.11-english.zip',
     '4.4.12' => 'https://files.phpmyadmin.net/phpMyAdmin/4.4.12/phpMyAdmin-4.4.12-english.zip',
     '4.4.14' => 'https://files.phpmyadmin.net/phpMyAdmin/4.4.14/phpMyAdmin-4.4.14-english.zip',
-    '4.5.0' => 'https://files.phpmyadmin.net/phpMyAdmin/4.5.0/phpMyAdmin-4.5.0-english.zip',
+    '4.4.15' => 'https://files.phpmyadmin.net/phpMyAdmin/4.4.15/phpMyAdmin-4.4.15-english.zip',
     'latest' => array(
-      'version' => '4.5.0',
-      'url' => 'https://files.phpmyadmin.net/phpMyAdmin/4.5.0/phpMyAdmin-4.5.0-english.zip',
+      'version' => '4.4.15',
+      'url' => 'https://files.phpmyadmin.net/phpMyAdmin/4.4.15/phpMyAdmin-4.4.15-english.zip',
     ),
   ),
   'pickle' => array(
