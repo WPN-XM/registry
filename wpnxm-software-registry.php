9009e755d6b03c56939f7eb94c12e68fa0d7e491
jakoch <jakoch@web.de>
2015-10-30 11:53:36 +0100

    updated software registry - PHP x86 v5.6.15

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Thu, 29 Oct 2015 12:54:45 +0000.
+    * Last Update Fri, 30 Oct 2015 10:53:34 +0000.
     * Do not edit manually!
     */
 
@@ -600,6 +600,15 @@
       'url' => 'http://nginx.org/download/nginx-1.9.4.zip',
     ),
   ),
+  'ngrok' => array(
+    'name' => 'ngrok x32',
+    'website' => 'https://ngrok.com/',
+    '0.0.0' => '',
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
   'node' => array(
     'name' => 'NodeJS x32',
     'website' => 'http://nodejs.org/',
@@ -1074,10 +1083,11 @@
     '5.6.11' => 'http://windows.php.net/downloads/releases/archives/php-5.6.11-nts-Win32-VC11-x86.zip',
     '5.6.12' => 'http://windows.php.net/downloads/releases/archives/php-5.6.12-nts-Win32-VC11-x86.zip',
     '5.6.13' => 'http://windows.php.net/downloads/releases/archives/php-5.6.13-nts-Win32-VC11-x86.zip',
-    '5.6.14' => 'http://windows.php.net/downloads/releases/php-5.6.14-nts-Win32-VC11-x86.zip',
+    '5.6.14' => 'http://windows.php.net/downloads/releases/archives/php-5.6.14-nts-Win32-VC11-x86.zip',
+    '5.6.15' => 'http://windows.php.net/downloads/releases/php-5.6.15-nts-Win32-VC11-x86.zip',
     'latest' => array(
-      'version' => '5.6.14',
-      'url' => 'http://windows.php.net/downloads/releases/php-5.6.14-nts-Win32-VC11-x86.zip',
+      'version' => '5.6.15',
+      'url' => 'http://windows.php.net/downloads/releases/php-5.6.15-nts-Win32-VC11-x86.zip',
     ),
   ),
   'php-cs-fixer' => array(
