c68740539db9c437aef11b9ded28f43bef72ced1
jakoch <jakoch@web.de>
2015-11-29 20:54:54 +0100

    updated software registry - PHP x86 v5.6.16

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Sun, 29 Nov 2015 19:54:46 +0000.
+    * Last Update Sun, 29 Nov 2015 19:54:52 +0000.
     * Do not edit manually!
     */
 
@@ -1228,10 +1228,11 @@
     '5.6.12' => 'http://windows.php.net/downloads/releases/archives/php-5.6.12-nts-Win32-VC11-x86.zip',
     '5.6.13' => 'http://windows.php.net/downloads/releases/archives/php-5.6.13-nts-Win32-VC11-x86.zip',
     '5.6.14' => 'http://windows.php.net/downloads/releases/archives/php-5.6.14-nts-Win32-VC11-x86.zip',
-    '5.6.15' => 'http://windows.php.net/downloads/releases/php-5.6.15-nts-Win32-VC11-x86.zip',
+    '5.6.15' => 'http://windows.php.net/downloads/releases/archives/php-5.6.15-nts-Win32-VC11-x86.zip',
+    '5.6.16' => 'http://windows.php.net/downloads/releases/php-5.6.16-nts-Win32-VC11-x86.zip',
     'latest' => array(
-      'version' => '5.6.15',
-      'url' => 'http://windows.php.net/downloads/releases/php-5.6.15-nts-Win32-VC11-x86.zip',
+      'version' => '5.6.16',
+      'url' => 'http://windows.php.net/downloads/releases/php-5.6.16-nts-Win32-VC11-x86.zip',
     ),
   ),
   'php-cs-fixer' => array(
