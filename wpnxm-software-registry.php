b154db2ba78e5b679389f2a85462a5ba301ca7a3
jakoch <jakoch@web.de>
2016-02-03 00:42:39 +0100

    updated software registry - Go Git Service x64 v0.8.25

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPN-XM Software Registry
     * ------------------------
-    * Last Update Tue, 02 Feb 2016 23:42:29 +0000.
+    * Last Update Tue, 02 Feb 2016 23:42:36 +0000.
     * Do not edit manually!
     */
 
@@ -216,9 +216,10 @@
     '0.7.33' => 'https://github.com/gogits/gogs/releases/download/v0.7.33/windows_amd64.zip',
     '0.8.0' => 'https://github.com/gogits/gogs/releases/download/v0.8.0/windows_amd64.zip',
     '0.8.10' => 'https://github.com/gogits/gogs/releases/download/v0.8.10/windows_amd64.zip',
+    '0.8.25' => 'https://github.com/gogits/gogs/releases/download/v0.8.25/windows_amd64.zip',
     'latest' => array(
-      'version' => '0.8.10',
-      'url' => 'https://github.com/gogits/gogs/releases/download/v0.8.10/windows_amd64.zip',
+      'version' => '0.8.25',
+      'url' => 'https://github.com/gogits/gogs/releases/download/v0.8.25/windows_amd64.zip',
     ),
   ),
   'gogs-x86' => array(
