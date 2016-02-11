4765c32679dc1e15e17f43b9e17d073580c51125
jakoch <jakoch@web.de>
2016-02-11 22:31:46 +0100

    added PHP software registry - issue https://github.com/WPN-XM/WPN-XM/issues/538

---
+++ wpnxm-php-software-registry.php
@@ -0,0 +1,709 @@
+<?php
+   /**
+    * WPИ-XM Server Stack
+    * Copyright © 2010 - 2016 Jens-André Koch <jakoch@web.de>
+    * http://wpn-xm.org/
+    *
+    * This source file is subject to the terms of the MIT license.
+    * For full copyright and license information, view the bundled LICENSE file.
+    */
+
+   /**
+    * WPИ-XM Server Stack - PHP Software Registry
+    * -------------------------------------------
+    * Last Update Thu, 11 Feb 2016 00:12:43 +0000.
+    * Do not edit manually!
+    */
+
+ return array(
+  'backdrop' => array(
+    'name' => 'Backdrop',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'cakephp' => array(
+    'name' => 'CakePHP',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'concrete5' => array(
+    'name' => 'concrete5',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'craft' => array(
+    'name' => 'craft',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'dokuwiki' => array(
+    'name' => 'Dokuwiki',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'drupal' => array(
+    'name' => 'Drupal',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'ez-publish' => array(
+    'name' => 'eZ Publish',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'fuelphp' => array(
+    'name' => 'FuelPHP',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'joomla' => array(
+    'name' => 'Joomla',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'laravel' => array(
+    'name' => 'Laravel',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'lithium' => array(
+    'name' => 'Lithium',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'magento' => array(
+    'name' => 'Magento',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'mediawiki' => array(
+    'name' => 'MediaWiki',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'neos' => array(
+    'name' => 'Neos',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'october' => array(
+    'name' => 'october',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'oxid' => array(
+    'name' => 'OXID',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'phpbb' => array(
+    'name' => 'phpBB',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'ppi-framework' => array(
+    'name' => 'PPI Framework',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'prestashop' => array(
+    'name' => 'PrestaShop',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'redaxo' => array(
+    'name' => 'redaxo',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'sculpin' => array(
+    'name' => 'sculpin',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'shopware' => array(
+    'name' => 'shopware',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'silverstripe' => array(
+    'name' => 'SilverStripe',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'symfony-2' => array(
+    'name' => 'Symfony 2',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'symfony-3' => array(
+    'name' => 'Symfony 3',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'typo3' => array(
+    'name' => 'TYPO3',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'wordpress' => array(
+    'name' => 'Wordpress',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'zend-apigility' => array(
+    'name' => 'Zend Apigility',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'zend-framework-1' => array(
+    'name' => 'Zend Framework 1',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+  'zend-framework-2' => array(
+    'name' => 'Zend Framework 2',
+    'website' => '',
+    'description' => '',
+    'category' => '',
+    'tags' => array(
+    ),
+    'requirements' => array(
+      'php_version' => '',
+      'php_extensions' => '',
+    ),
+    'github' => '',
+    'license' => '',
+    'logo' => '',
+    'screenshots' => array(
+    ),
+    'versions' => array(
+    ),
+    'latest' => array(
+      'version' => '',
+      'url' => '',
+    ),
+  ),
+);
