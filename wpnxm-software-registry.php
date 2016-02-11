11ccb4792ec123d0d53520531b90aa1ed4dec374
jakoch <jakoch@web.de>
2016-02-11 22:23:07 +0100

    updated software registry - Git for Windows v2.7.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 11 Feb 2016 21:22:58 +0000.
+    * Last Update Thu, 11 Feb 2016 21:23:03 +0000.
     * Do not edit manually!
     */
 
@@ -552,9 +552,10 @@
     '2.6.3' => 'https://github.com/git-for-windows/git/releases/download/v2.6.3.windows.1/PortableGit-2.6.3-32-bit.7z.exe',
     '2.6.4' => 'https://github.com/git-for-windows/git/releases/download/v2.6.4.windows.1/PortableGit-2.6.4-32-bit.7z.exe',
     '2.7.0' => 'https://github.com/git-for-windows/git/releases/download/v2.7.0.windows.1/PortableGit-2.7.0-32-bit.7z.exe',
+    '2.7.1' => 'https://github.com/git-for-windows/git/releases/download/v2.7.1.windows.1/PortableGit-2.7.1-32-bit.7z.exe',
     'latest' => array(
-      'version' => '2.7.0',
-      'url' => 'https://github.com/git-for-windows/git/releases/download/v2.7.0.windows.1/PortableGit-2.7.0-32-bit.7z.exe',
+      'version' => '2.7.1',
+      'url' => 'https://github.com/git-for-windows/git/releases/download/v2.7.1.windows.1/PortableGit-2.7.1-32-bit.7z.exe',
     ),
   ),
   'msysgit-x64' => array(
