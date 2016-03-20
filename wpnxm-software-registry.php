4e4423472d1e66208d4aefb935efb92aa6fdfd06
jakoch <jakoch@web.de>
2016-03-20 11:31:38 +0100

    updated software registry - PHP Extension OAuth v2.0.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sun, 20 Mar 2016 10:31:29 +0000.
+    * Last Update Sun, 20 Mar 2016 10:31:35 +0000.
     * Do not edit manually!
     */
 
@@ -3127,14 +3127,22 @@
         '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-VC14-x64.zip',
       ),
     ),
+    '2.0.1' => array(
+      'x86' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.1/php_oauth-2.0.1-7.0-nts-VC14-x86.zip',
+      ),
+      'x64' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.1/php_oauth-2.0.1-7.0-nts-VC14-x64.zip',
+      ),
+    ),
     'latest' => array(
-      'version' => '2.0.0',
+      'version' => '2.0.1',
       'url' => array(
         'x86' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-VC14-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.1/php_oauth-2.0.1-7.0-nts-VC14-x86.zip',
         ),
         'x64' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-VC14-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.1/php_oauth-2.0.1-7.0-nts-VC14-x64.zip',
         ),
       ),
     ),
