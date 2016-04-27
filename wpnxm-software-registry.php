80326c92a84d6809f665a726c8627800d1c7680f
jakoch <jakoch@web.de>
2016-04-27 15:21:13 +0200

    updated software registry - ElasticSearch v2.3.2

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Wed, 27 Apr 2016 13:16:56 +0000.
+    * Last Update Wed, 27 Apr 2016 13:21:10 +0000.
     * Do not edit manually!
     */
 
@@ -257,9 +257,10 @@
     '2.2.1' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.2.1/elasticsearch-2.2.1.zip',
     '2.3.0' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.0/elasticsearch-2.3.0.zip',
     '2.3.1' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.1/elasticsearch-2.3.1.zip',
+    '2.3.2' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.2/elasticsearch-2.3.2.zip',
     'latest' => array(
-      'version' => '2.3.1',
-      'url' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.1/elasticsearch-2.3.1.zip',
+      'version' => '2.3.2',
+      'url' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/2.3.2/elasticsearch-2.3.2.zip',
     ),
   ),
   'firebird' => array(
