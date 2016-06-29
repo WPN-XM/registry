e6909863c49c7de0d7a30ae8b890e39bf8228c4d
jakoch <jakoch@web.de>
2016-06-29 13:32:32 +0200

    updated software registry - Composer vlatest

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 24 Jun 2016 10:52:37 +0000.
+    * Last Update Wed, 29 Jun 2016 11:32:28 +0000.
     * Do not edit manually!
     */
 
@@ -187,6 +187,7 @@
     'name' => 'Composer',
     'website' => 'http://getcomposer.org/',
     '1.1.2' => 'https://getcomposer.org/download/1.1.2/composer.phar',
+    '1.1.3' => 'https://getcomposer.org/download/1.1.3/composer.phar',
     'latest' => array(
       'version' => 'latest',
       'url' => 'https://getcomposer.org/composer.phar',
