b6e95dc3146fd5396e7834ec506d707708e49dea
jakoch <jakoch@web.de>
2016-07-10 14:22:32 +0200

    updated software registry - MariaDB x86 v10.2.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sun, 10 Jul 2016 12:22:23 +0000.
+    * Last Update Sun, 10 Jul 2016 12:22:28 +0000.
     * Do not edit manually!
     */
 
@@ -475,9 +475,10 @@
     '10.1.12' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.1.12/win32-packages/mariadb-10.1.12-win32.zip',
     '10.1.13' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.1.13/win32-packages/mariadb-10.1.13-win32.zip',
     '10.2.0' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.0/win32-packages/mariadb-10.2.0-win32.zip',
+    '10.2.1' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.1/win32-packages/mariadb-10.2.1-win32.zip',
     'latest' => array(
-      'version' => '10.2.0',
-      'url' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.0/win32-packages/mariadb-10.2.0-win32.zip',
+      'version' => '10.2.1',
+      'url' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.1/win32-packages/mariadb-10.2.1-win32.zip',
     ),
   ),
   'mariadb-x64' => array(
