92bf8ca42d4e2ca4525b70e6012a90698bb55528
jakoch <jakoch@web.de>
2016-08-23 00:01:47 +0200

    updated software registry - PHP Extension Couchbase v2.2.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 22 Aug 2016 22:01:38 +0000.
+    * Last Update Mon, 22 Aug 2016 22:01:43 +0000.
     * Do not edit manually!
     */
 
@@ -2099,23 +2099,40 @@
         '5.4' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.4-nts-vc9-x86.zip',
         '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.5-nts-vc11-x86.zip',
         '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.6-nts-vc11-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-7.0-nts-vc14-x86.zip',
       ),
       'x64' => array(
         '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.5-nts-vc11-x64.zip',
         '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.6-nts-vc11-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-7.0-nts-vc14-x64.zip',
+      ),
+    ),
+    '2.2.1' => array(
+      'x86' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.4-nts-vc9-x86.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.5-nts-vc11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.6-nts-vc11-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-7.0-nts-vc14-x86.zip',
+      ),
+      'x64' => array(
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.5-nts-vc11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.6-nts-vc11-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-7.0-nts-vc14-x64.zip',
       ),
     ),
     'latest' => array(
-      'version' => '2.2.0',
+      'version' => '2.2.1',
       'url' => array(
         'x86' => array(
-          '5.4' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.4-nts-vc9-x86.zip',
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.5-nts-vc11-x86.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.6-nts-vc11-x86.zip',
+          '5.4' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.4-nts-vc9-x86.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.5-nts-vc11-x86.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.6-nts-vc11-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-7.0-nts-vc14-x86.zip',
         ),
         'x64' => array(
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.5-nts-vc11-x64.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.0/php_couchbase-2.2.0-5.6-nts-vc11-x64.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.5-nts-vc11-x64.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-5.6-nts-vc11-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/couchbase/2.2.1/php_couchbase-2.2.1-7.0-nts-vc14-x64.zip',
         ),
       ),
     ),
