1b4b390fd2ca2bccd486470e53427041317ebebd
jakoch <jakoch@web.de>
2016-09-04 13:01:42 +0200

    updated software registry - PHP QA x64 v7.1.0RC1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sun, 04 Sep 2016 11:01:34 +0000.
+    * Last Update Sun, 04 Sep 2016 11:01:39 +0000.
     * Do not edit manually!
     */
 
@@ -1667,10 +1667,11 @@
     '7.1.0alpha3' => 'http://windows.php.net/downloads/qa/archives/php-7.1.0alpha3-nts-Win32-VC14-x64.zip',
     '7.1.0beta1' => 'http://windows.php.net/downloads/qa/archives/php-7.1.0beta1-nts-Win32-VC14-x64.zip',
     '7.1.0beta2' => 'http://windows.php.net/downloads/qa/archives/php-7.1.0beta2-nts-Win32-VC14-x64.zip',
-    '7.1.0beta3' => 'http://windows.php.net/downloads/qa/php-7.1.0beta3-nts-Win32-VC14-x64.zip',
+    '7.1.0beta3' => 'http://windows.php.net/downloads/qa/archives/php-7.1.0beta3-nts-Win32-VC14-x64.zip',
+    '7.1.0RC1' => 'http://windows.php.net/downloads/qa/php-7.1.0RC1-nts-Win32-VC14-x64.zip',
     'latest' => array(
-      'version' => '7.1.0beta3',
-      'url' => 'http://windows.php.net/downloads/qa/php-7.1.0beta3-nts-Win32-VC14-x64.zip',
+      'version' => '7.1.0RC1',
+      'url' => 'http://windows.php.net/downloads/qa/php-7.1.0RC1-nts-Win32-VC14-x64.zip',
     ),
   ),
   'php-x64' => array(
