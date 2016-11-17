9d327b823f7b0faf2fd64b5f9bd83614b5ad2a7d
jakoch <jakoch@web.de>
2016-11-17 12:40:56 +0100

    updated software registry - PHP Extension Wincache v2.0.0.8

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 17 Nov 2016 11:40:46 +0000.
+    * Last Update Thu, 17 Nov 2016 11:40:52 +0000.
     * Do not edit manually!
     */
 
@@ -4992,20 +4992,28 @@
     ),
     '2.0.0.6' => array(
       'x86' => array(
-        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-VC14-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-vc14-x86.zip',
       ),
       'x64' => array(
-        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-VC14-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-vc14-x64.zip',
+      ),
+    ),
+    '2.0.0.8' => array(
+      'x86' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.8/php_wincache-2.0.0.8-7.0-nts-vc14-x86.zip',
+      ),
+      'x64' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.8/php_wincache-2.0.0.8-7.0-nts-vc14-x64.zip',
       ),
     ),
     'latest' => array(
-      'version' => '2.0.0.6',
+      'version' => '2.0.0.8',
       'url' => array(
         'x86' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-VC14-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.8/php_wincache-2.0.0.8-7.0-nts-vc14-x86.zip',
         ),
         'x64' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.6/php_wincache-2.0.0.6-7.0-nts-VC14-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/wincache/2.0.0.8/php_wincache-2.0.0.8-7.0-nts-vc14-x64.zip',
         ),
       ),
     ),
