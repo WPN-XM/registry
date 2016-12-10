25c53a02f24dc371d50fb90976011a420c3b281f
jakoch <jakoch@web.de>
2016-12-10 02:24:29 +0100

    updated software registry - PHP Extension OAuth v2.0.2

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sat, 10 Dec 2016 01:23:43 +0000.
+    * Last Update Sat, 10 Dec 2016 01:24:25 +0000.
     * Do not edit manually!
     */
 
@@ -3698,21 +3698,18 @@
     ),
     '1.2.3' => array(
       'x86' => array(
-        '5.4' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.4-nts-VC9-x86.zip',
-        '5.5' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.5-nts-VC11-x86.zip',
-        '5.6' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.6-nts-VC11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.6-nts-vc11-x86.zip',
       ),
       'x64' => array(
-        '5.5' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.5-nts-VC11-x64.zip',
-        '5.6' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.6-nts-VC11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/oauth/1.2.3/php_oauth-1.2.3-5.6-nts-vc11-x64.zip',
       ),
     ),
     '2.0.0' => array(
       'x86' => array(
-        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-VC14-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-vc14-x86.zip',
       ),
       'x64' => array(
-        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-VC14-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.0/php_oauth-2.0.0-7.0-nts-vc14-x64.zip',
       ),
     ),
     '2.0.1' => array(
@@ -3724,15 +3721,25 @@
       ),
     ),
     '2.0.2' => array(
+      'x86' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.0-nts-vc14-x86.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.1-nts-vc14-x86.zip',
+      ),
       'x64' => array(
         '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.0-nts-vc14-x64.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.1-nts-vc14-x64.zip',
       ),
     ),
     'latest' => array(
       'version' => '2.0.2',
       'url' => array(
+        'x86' => array(
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.0-nts-vc14-x86.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.1-nts-vc14-x86.zip',
+        ),
         'x64' => array(
           '7.0' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.0-nts-vc14-x64.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/oauth/2.0.2/php_oauth-2.0.2-7.1-nts-vc14-x64.zip',
         ),
       ),
     ),
