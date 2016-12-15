eefd06ce2677e53a0f9c9e51d223ee91fd7ff012
jakoch <jakoch@web.de>
2016-12-15 12:11:16 +0100

    updated software registry - PHP Extension Redis v3.1.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 15 Dec 2016 11:11:07 +0000.
+    * Last Update Thu, 15 Dec 2016 11:11:13 +0000.
     * Do not edit manually!
     */
 
@@ -4134,14 +4134,22 @@
         '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.0.0/php_redis-3.0.0-7.0-nts-vc14-x64.zip',
       ),
     ),
+    '3.1.0' => array(
+      'x86' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.1.0/php_redis-3.1.0-7.0-nts-vc14-x86.zip',
+      ),
+      'x64' => array(
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.1.0/php_redis-3.1.0-7.0-nts-vc14-x64.zip',
+      ),
+    ),
     'latest' => array(
-      'version' => '3.0.0',
+      'version' => '3.1.0',
       'url' => array(
         'x86' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.0.0/php_redis-3.0.0-7.0-nts-vc14-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.1.0/php_redis-3.1.0-7.0-nts-vc14-x86.zip',
         ),
         'x64' => array(
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.0.0/php_redis-3.0.0-7.0-nts-vc14-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/redis/3.1.0/php_redis-3.1.0-7.0-nts-vc14-x64.zip',
         ),
       ),
     ),
