32093c086f2e54b303ffa42161a27259224358e8
jakoch <jakoch@web.de>
2016-12-23 00:20:04 +0100

    updated software registry - Imagick x86 v7.0.4-0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 22 Dec 2016 23:19:56 +0000.
+    * Last Update Thu, 22 Dec 2016 23:20:01 +0000.
     * Do not edit manually!
     */
 
@@ -430,9 +430,10 @@
     '7.0.3-8' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-8-portable-Q16-x86.zip',
     '7.0.3-9' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-9-portable-Q16-x86.zip',
     '7.0.3-10' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-10-portable-Q16-x86.zip',
+    '7.0.4-0' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.4-0-portable-Q16-x86.zip',
     'latest' => array(
-      'version' => '7.0.3-10',
-      'url' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.3-10-portable-Q16-x86.zip',
+      'version' => '7.0.4-0',
+      'url' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.4-0-portable-Q16-x86.zip',
     ),
   ),
   'imagick-x64' => array(
