7277bc6f524f251f57f64a7805d182c61c1956de
jakoch <jakoch@web.de>
2017-01-19 11:47:55 +0100

    updated software registry - ElasticSearch v5.1.2

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 19 Jan 2017 10:47:50 +0000.
+    * Last Update Thu, 19 Jan 2017 10:47:54 +0000.
     * Do not edit manually!
     */
 
@@ -334,9 +334,10 @@
     '5.0.1' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.0.1/elasticsearch-5.0.1.zip',
     '5.0.2' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.0.2/elasticsearch-5.0.2.zip',
     '5.1.1' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.1.1/elasticsearch-5.1.1.zip',
+    '5.1.2' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.1.2/elasticsearch-5.1.2.zip',
     'latest' => array(
-      'version' => '5.1.1',
-      'url' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.1.1/elasticsearch-5.1.1.zip',
+      'version' => '5.1.2',
+      'url' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/zip/elasticsearch/5.1.2/elasticsearch-5.1.2.zip',
     ),
   ),
   'firebird' => array(
