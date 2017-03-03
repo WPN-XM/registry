3a3e4f89b752885295cc082274dfa7e001e14666
jakoch <jakoch@web.de>
2017-03-03 14:38:43 +0100

    updated software registry - Git for Windows x64 v2.12.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 03 Mar 2017 13:38:39 +0000.
+    * Last Update Fri, 03 Mar 2017 13:38:42 +0000.
     * Do not edit manually!
     */
 
@@ -849,9 +849,10 @@
     '2.10.2' => 'https://github.com/git-for-windows/git/releases/download/v2.10.2.windows.1/PortableGit-2.10.2-64-bit.7z.exe',
     '2.11.0' => 'https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/PortableGit-2.11.0-64-bit.7z.exe',
     '2.11.1' => 'https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/PortableGit-2.11.1-64-bit.7z.exe',
+    '2.12.0' => 'https://github.com/git-for-windows/git/releases/download/v2.12.0.windows.1/PortableGit-2.12.0-64-bit.7z.exe',
     'latest' => array(
-      'version' => '2.11.1',
-      'url' => 'https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/PortableGit-2.11.1-64-bit.7z.exe',
+      'version' => '2.12.0',
+      'url' => 'https://github.com/git-for-windows/git/releases/download/v2.12.0.windows.1/PortableGit-2.12.0-64-bit.7z.exe',
     ),
   ),
   'neo4j-x64' => array(
