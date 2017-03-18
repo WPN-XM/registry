74af49a526bd4d31316497007ff0515042c1f243
jakoch <jakoch@web.de>
2017-03-18 18:33:41 +0100

    updated software registry - NodeJS x32 v7.7.3

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sat, 18 Mar 2017 17:33:36 +0000.
+    * Last Update Sat, 18 Mar 2017 17:33:40 +0000.
     * Do not edit manually!
     */
 
@@ -1202,9 +1202,10 @@
     '7.7.0' => 'http://nodejs.org/dist/v7.7.0/win-x86/node.exe',
     '7.7.1' => 'http://nodejs.org/dist/v7.7.1/win-x86/node.exe',
     '7.7.2' => 'http://nodejs.org/dist/v7.7.2/win-x86/node.exe',
+    '7.7.3' => 'http://nodejs.org/dist/v7.7.3/win-x86/node.exe',
     'latest' => array(
-      'version' => '7.7.2',
-      'url' => 'http://nodejs.org/dist/v7.7.2/win-x86/node.exe',
+      'version' => '7.7.3',
+      'url' => 'http://nodejs.org/dist/v7.7.3/win-x86/node.exe',
     ),
   ),
   'node-x64' => array(
