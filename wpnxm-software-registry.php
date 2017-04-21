5b0465e197943979602e0792220e965ffe1c33fc
jakoch <jakoch@web.de>
2017-04-21 03:34:21 +0200

    updated software registry - PHP Extension JsonD v1.4.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 21 Apr 2017 01:34:16 +0000.
+    * Last Update Fri, 21 Apr 2017 01:34:19 +0000.
     * Do not edit manually!
     */
 
@@ -3329,26 +3329,43 @@
     'website' => 'http://php.net/jsond',
     '1.3.0' => array(
       'x86' => array(
-        '5.4' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.4-nts-VC9-x86.zip',
-        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-VC11-x86.zip',
-        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-VC11-x86.zip',
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.4-nts-vc9-x86.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-vc11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-vc11-x86.zip',
       ),
       'x64' => array(
-        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-VC11-x64.zip',
-        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-VC11-x64.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-vc11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-vc11-x64.zip',
+      ),
+    ),
+    '1.4.0' => array(
+      'x86' => array(
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.5-nts-vc11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.6-nts-vc11-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.0-nts-vc14-x86.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.1-nts-vc14-x86.zip',
+      ),
+      'x64' => array(
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.5-nts-vc11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.6-nts-vc11-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.0-nts-vc14-x64.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.1-nts-vc14-x64.zip',
       ),
     ),
     'latest' => array(
-      'version' => '1.3.0',
+      'version' => '1.4.0',
       'url' => array(
         'x86' => array(
-          '5.4' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.4-nts-VC9-x86.zip',
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-VC11-x86.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-VC11-x86.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.5-nts-vc11-x86.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.6-nts-vc11-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.0-nts-vc14-x86.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.1-nts-vc14-x86.zip',
         ),
         'x64' => array(
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.5-nts-VC11-x64.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.3.0/php_jsond-1.3.0-5.6-nts-VC11-x64.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.5-nts-vc11-x64.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-5.6-nts-vc11-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.0-nts-vc14-x64.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/jsond/1.4.0/php_jsond-1.4.0-7.1-nts-vc14-x64.zip',
         ),
       ),
     ),
