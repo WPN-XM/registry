9ffd0fc4035b2e06544a797645567892dde80433
jakoch <jakoch@web.de>
2017-04-21 03:33:51 +0200

    updated software registry - MariaDB x64 v10.3.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 21 Apr 2017 01:33:43 +0000.
+    * Last Update Fri, 21 Apr 2017 01:33:49 +0000.
     * Do not edit manually!
     */
 
@@ -724,9 +724,11 @@
     '10.2.2' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.2/winx64-packages/mariadb-10.2.2-winx64.zip',
     '10.2.3' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.3/winx64-packages/mariadb-10.2.3-winx64.zip',
     '10.2.4' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.4/winx64-packages/mariadb-10.2.4-winx64.zip',
+    '10.2.5' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.5/winx64-packages/mariadb-10.2.5-winx64.zip',
+    '10.3.0' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.3.0/winx64-packages/mariadb-10.3.0-winx64.zip',
     'latest' => array(
-      'version' => '10.2.4',
-      'url' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.2.4/winx64-packages/mariadb-10.2.4-winx64.zip',
+      'version' => '10.3.0',
+      'url' => 'http://mirror.jmu.edu/pub/mariadb/mariadb-10.3.0/winx64-packages/mariadb-10.3.0-winx64.zip',
     ),
   ),
   'memadmin' => array(
