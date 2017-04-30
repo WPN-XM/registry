c0ad5f057a14e45df714a056e50f12fb7380bc98
jakoch <jakoch@web.de>
2017-04-30 18:21:09 +0200

    updated software registry - FileZilla FTP Client x86 v3.25.2

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sun, 30 Apr 2017 16:21:05 +0000.
+    * Last Update Sun, 30 Apr 2017 16:21:08 +0000.
     * Do not edit manually!
     */
 
@@ -421,9 +421,10 @@
     'name' => 'FileZilla FTP Client x86',
     'website' => 'https://filezilla-project.org/',
     '3.25.1' => 'https://download.filezilla-project.org/client/FileZilla_3.25.1_win32-setup_bundled2.exe',
+    '3.25.2' => 'https://download.filezilla-project.org/client/FileZilla_3.25.2_win32-setup_bundled2.exe',
     'latest' => array(
-      'version' => '3.25.1',
-      'url' => 'https://download.filezilla-project.org/client/FileZilla_3.25.1_win32-setup_bundled2.exe',
+      'version' => '3.25.2',
+      'url' => 'https://download.filezilla-project.org/client/FileZilla_3.25.2_win32-setup_bundled2.exe',
     ),
   ),
   'firebird' => array(
