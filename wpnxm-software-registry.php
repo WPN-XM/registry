0dad5683c1dbc2e233d8929233e3b7a90209c553
jakoch <jakoch@web.de>
2017-08-14 08:38:12 +0200

    updated software registry - Imagick x64 v7.0.6-7

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 14 Aug 2017 06:38:10 +0000.
+    * Last Update Mon, 14 Aug 2017 06:38:11 +0000.
     * Do not edit manually!
     */
 
@@ -690,9 +690,10 @@
     '7.0.5-9' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.5-9-portable-Q16-x64.zip',
     '7.0.5-10' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.5-10-portable-Q16-x64.zip',
     '7.0.6-0' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.6-0-portable-Q16-x64.zip',
+    '7.0.6-7' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.6-7-portable-Q16-x64.zip',
     'latest' => array(
-      'version' => '7.0.6-0',
-      'url' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.6-0-portable-Q16-x64.zip',
+      'version' => '7.0.6-7',
+      'url' => 'http://www.imagemagick.org/download/binaries/ImageMagick-7.0.6-7-portable-Q16-x64.zip',
     ),
   ),
   'influxdb-x64' => array(
