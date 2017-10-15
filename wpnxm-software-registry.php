dcece208e8b604563e8fd8df52ce379fb0fc85ae
jakoch <jakoch@web.de>
2017-10-15 11:43:34 +0200

    updated software registry - PostgreSQL x64 v10.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sun, 15 Oct 2017 09:43:27 +0000.
+    * Last Update Sun, 15 Oct 2017 09:43:33 +0000.
     * Do not edit manually!
     */
 
@@ -6806,9 +6806,10 @@
     '9.6.3' => 'http://get.enterprisedb.com/postgresql/postgresql-9.6.3-1-windows-x64-binaries.zip',
     '9.6.4' => 'http://get.enterprisedb.com/postgresql/postgresql-9.6.4-1-windows-x64-binaries.zip',
     '9.6.5' => 'http://get.enterprisedb.com/postgresql/postgresql-9.6.5-1-windows-x64-binaries.zip',
+    '10.0' => 'http://get.enterprisedb.com/postgresql/postgresql-10.0-1-windows-x64-binaries.zip',
     'latest' => array(
-      'version' => '9.6.5',
-      'url' => 'http://get.enterprisedb.com/postgresql/postgresql-9.6.5-1-windows-x64-binaries.zip',
+      'version' => '10.0',
+      'url' => 'http://get.enterprisedb.com/postgresql/postgresql-10.0-1-windows-x64-binaries.zip',
     ),
   ),
   'qcachegrind-x64' => array(
