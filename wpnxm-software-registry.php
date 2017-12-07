951e80348383d3ba11b34164016277e9d9693d28
jakoch <jakoch@web.de>
2017-12-07 01:21:03 +0100

    updated software registry - Go Git Service x86 v0.11.34

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Thu, 07 Dec 2017 00:21:00 +0000.
+    * Last Update Thu, 07 Dec 2017 00:21:02 +0000.
     * Do not edit manually!
     */
 
@@ -575,9 +575,10 @@
     '0.11.4' => 'https://github.com/gogits/gogs/releases/download/v0.11.4/windows_386_mws.zip',
     '0.11.19' => 'https://github.com/gogits/gogs/releases/download/v0.11.19/windows_386_mws.zip',
     '0.11.29' => 'https://github.com/gogits/gogs/releases/download/v0.11.29/windows_386_mws.zip',
+    '0.11.34' => 'https://github.com/gogits/gogs/releases/download/v0.11.34/windows_386_mws.zip',
     'latest' => array(
-      'version' => '0.11.29',
-      'url' => 'https://github.com/gogits/gogs/releases/download/v0.11.29/windows_386_mws.zip',
+      'version' => '0.11.34',
+      'url' => 'https://github.com/gogits/gogs/releases/download/v0.11.34/windows_386_mws.zip',
     ),
   ),
   'graphviz' => array(
