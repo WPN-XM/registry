a8edf76a5867be984f794a0bf4052d172a73b17f
jakoch <jakoch@web.de>
2018-04-13 14:06:14 +0200

    updated software registry - RClone x64 v1.40

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 13 Apr 2018 12:05:59 +0000.
+    * Last Update Fri, 13 Apr 2018 12:06:13 +0000.
     * Do not edit manually!
     */
 
@@ -7221,9 +7221,10 @@
     '1.37' => 'https://github.com/ncw/rclone/releases/download/v1.37/rclone-v1.37-windows-amd64.zip',
     '1.38' => 'https://github.com/ncw/rclone/releases/download/v1.38/rclone-v1.38-windows-amd64.zip',
     '1.39' => 'https://github.com/ncw/rclone/releases/download/v1.39/rclone-v1.39-windows-amd64.zip',
+    '1.40' => 'https://github.com/ncw/rclone/releases/download/v1.40/rclone-v1.40-windows-amd64.zip',
     'latest' => array(
-      'version' => '1.39',
-      'url' => 'https://github.com/ncw/rclone/releases/download/v1.39/rclone-v1.39-windows-amd64.zip',
+      'version' => '1.40',
+      'url' => 'https://github.com/ncw/rclone/releases/download/v1.40/rclone-v1.40-windows-amd64.zip',
     ),
   ),
   'rclone-x86' => array(
