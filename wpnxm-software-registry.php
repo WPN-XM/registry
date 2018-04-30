8163f64fbf9037cdc63e25b23d276fab00bab94e
jakoch <jakoch@web.de>
2018-04-30 12:37:05 +0200

    updated software registry - Cayley x64 v0.7.3

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 30 Apr 2018 10:36:57 +0000.
+    * Last Update Mon, 30 Apr 2018 10:37:03 +0000.
     * Do not edit manually!
     */
 
@@ -225,9 +225,10 @@
     '0.6.1' => 'https://github.com/google/cayley/releases/download/v0.6.1/cayley_v0.6.1_windows_amd64.zip',
     '0.7.0' => 'https://github.com/google/cayley/releases/download/v0.7.0/cayley_v0.7.0_windows_amd64.zip',
     '0.7.2' => 'https://github.com/google/cayley/releases/download/v0.7.2/cayley_v0.7.2_windows_amd64.zip',
+    '0.7.3' => 'https://github.com/google/cayley/releases/download/v0.7.3/cayley_v0.7.3_windows_amd64.zip',
     'latest' => array(
-      'version' => '0.7.2',
-      'url' => 'https://github.com/google/cayley/releases/download/v0.7.2/cayley_v0.7.2_windows_amd64.zip',
+      'version' => '0.7.3',
+      'url' => 'https://github.com/google/cayley/releases/download/v0.7.3/cayley_v0.7.3_windows_amd64.zip',
     ),
   ),
   'chromedriver' => array(
