99fc8b5b13848da75bafad9537b8063b2916c3c5
jakoch <jakoch@web.de>
2018-04-30 12:38:02 +0200

    updated software registry - Strawberry Perl x64 v5.26.2.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 30 Apr 2018 10:37:58 +0000.
+    * Last Update Mon, 30 Apr 2018 10:38:01 +0000.
     * Do not edit manually!
     */
 
@@ -2054,9 +2054,10 @@
     '5.26.0.1' => 'http://strawberryperl.com/download/5.26.0.1/strawberry-perl-5.26.0.1-64bit.zip',
     '5.26.0.2' => 'http://strawberryperl.com/download/5.26.0.2/strawberry-perl-5.26.0.2-64bit.zip',
     '5.26.1.1' => 'http://strawberryperl.com/download/5.26.1.1/strawberry-perl-5.26.1.1-64bit.zip',
+    '5.26.2.1' => 'http://strawberryperl.com/download/5.26.2.1/strawberry-perl-5.26.2.1-64bit.zip',
     'latest' => array(
-      'version' => '5.26.1.1',
-      'url' => 'http://strawberryperl.com/download/5.26.1.1/strawberry-perl-5.26.1.1-64bit.zip',
+      'version' => '5.26.2.1',
+      'url' => 'http://strawberryperl.com/download/5.26.2.1/strawberry-perl-5.26.2.1-64bit.zip',
     ),
   ),
   'phantomjs' => array(
