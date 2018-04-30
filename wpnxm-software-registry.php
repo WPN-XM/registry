ec7060bbfe53f654125fb3066c458b2c0bd4f789
jakoch <jakoch@web.de>
2018-04-30 12:38:08 +0200

    updated software registry - PHP Extension Event v2.4.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 30 Apr 2018 10:38:04 +0000.
+    * Last Update Mon, 30 Apr 2018 10:38:07 +0000.
     * Do not edit manually!
     */
 
@@ -3419,32 +3419,58 @@
     ),
     '2.3.0' => array(
       'x86' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.4-nts-vc9-x86.zip',
         '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.5-nts-vc11-x86.zip',
         '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.6-nts-vc11-x86.zip',
         '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.0-nts-vc14-x86.zip',
         '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.1-nts-vc14-x86.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.2-nts-vc15-x86.zip',
       ),
       'x64' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.4-nts-vc9-x64.zip',
         '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.5-nts-vc11-x64.zip',
         '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.6-nts-vc11-x64.zip',
         '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.0-nts-vc14-x64.zip',
         '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.1-nts-vc14-x64.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.2-nts-vc15-x64.zip',
+      ),
+    ),
+    '2.4.0' => array(
+      'x86' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.4-nts-vc9-x86.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.5-nts-vc11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.6-nts-vc11-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.0-nts-vc14-x86.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.1-nts-vc14-x86.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.2-nts-vc15-x86.zip',
+      ),
+      'x64' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.4-nts-vc9-x64.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.5-nts-vc11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.6-nts-vc11-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.0-nts-vc14-x64.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.1-nts-vc14-x64.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.2-nts-vc15-x64.zip',
       ),
     ),
     'latest' => array(
-      'version' => '2.3.0',
+      'version' => '2.4.0',
       'url' => array(
         'x86' => array(
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.5-nts-vc11-x86.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.6-nts-vc11-x86.zip',
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.0-nts-vc14-x86.zip',
-          '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.1-nts-vc14-x86.zip',
+          '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.4-nts-vc9-x86.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.5-nts-vc11-x86.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.6-nts-vc11-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.0-nts-vc14-x86.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.1-nts-vc14-x86.zip',
+          '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.2-nts-vc15-x86.zip',
         ),
         'x64' => array(
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.5-nts-vc11-x64.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-5.6-nts-vc11-x64.zip',
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.0-nts-vc14-x64.zip',
-          '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.3.0/php_event-2.3.0-7.1-nts-vc14-x64.zip',
+          '5.4' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.4-nts-vc9-x64.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.5-nts-vc11-x64.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-5.6-nts-vc11-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.0-nts-vc14-x64.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.1-nts-vc14-x64.zip',
+          '7.2' => 'http://windows.php.net/downloads/pecl/releases/event/2.4.0/php_event-2.4.0-7.2-nts-vc15-x64.zip',
         ),
       ),
     ),
