902ab95a8d01fe09c70493608f3dd539cd5cbc77
jakoch <jakoch@web.de>
2018-06-09 17:33:04 +0200

    updated software registry - PostgreSQL x86 v10.4

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Sat, 09 Jun 2018 15:33:00 +0000.
+    * Last Update Sat, 09 Jun 2018 15:33:03 +0000.
     * Do not edit manually!
     */
 
@@ -8159,9 +8159,10 @@
     '10.0' => 'http://get.enterprisedb.com/postgresql/postgresql-10.0-1-windows-binaries.zip',
     '10.1' => 'http://get.enterprisedb.com/postgresql/postgresql-10.1-1-windows-binaries.zip',
     '10.3' => 'http://get.enterprisedb.com/postgresql/postgresql-10.3-1-windows-binaries.zip',
+    '10.4' => 'http://get.enterprisedb.com/postgresql/postgresql-10.4-1-windows-binaries.zip',
     'latest' => array(
-      'version' => '10.3',
-      'url' => 'http://get.enterprisedb.com/postgresql/postgresql-10.3-1-windows-binaries.zip',
+      'version' => '10.4',
+      'url' => 'http://get.enterprisedb.com/postgresql/postgresql-10.4-1-windows-binaries.zip',
     ),
   ),
   'postgresql-x64' => array(
