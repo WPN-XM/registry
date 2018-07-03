90207c03cc35c584522fdcb1d2e6c64e1f59905f
jakoch <jakoch@web.de>
2018-07-03 12:00:14 +0200

    updated software registry - PHP Extension Varnish v1.2.4

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Tue, 03 Jul 2018 10:00:09 +0000.
+    * Last Update Tue, 03 Jul 2018 10:00:13 +0000.
     * Do not edit manually!
     */
 
@@ -7347,24 +7347,42 @@
         '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.2-nts-vc15-x64.zip',
       ),
     ),
+    '1.2.4' => array(
+      'x86' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.4-nts-vc9-x86.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.5-nts-vc11-x86.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.6-nts-vc11-x86.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.0-nts-vc14-x86.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.1-nts-vc14-x86.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.2-nts-vc15-x86.zip',
+      ),
+      'x64' => array(
+        '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.4-nts-vc9-x64.zip',
+        '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.5-nts-vc11-x64.zip',
+        '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.6-nts-vc11-x64.zip',
+        '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.0-nts-vc14-x64.zip',
+        '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.1-nts-vc14-x64.zip',
+        '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.2-nts-vc15-x64.zip',
+      ),
+    ),
     'latest' => array(
-      'version' => '1.2.3',
+      'version' => '1.2.4',
       'url' => array(
         'x86' => array(
-          '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.4-nts-vc9-x86.zip',
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.5-nts-vc11-x86.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.6-nts-vc11-x86.zip',
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.0-nts-vc14-x86.zip',
-          '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.1-nts-vc14-x86.zip',
-          '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.2-nts-vc15-x86.zip',
+          '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.4-nts-vc9-x86.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.5-nts-vc11-x86.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.6-nts-vc11-x86.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.0-nts-vc14-x86.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.1-nts-vc14-x86.zip',
+          '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.2-nts-vc15-x86.zip',
         ),
         'x64' => array(
-          '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.4-nts-vc9-x64.zip',
-          '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.5-nts-vc11-x64.zip',
-          '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-5.6-nts-vc11-x64.zip',
-          '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.0-nts-vc14-x64.zip',
-          '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.1-nts-vc14-x64.zip',
-          '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.3/php_varnish-1.2.3-7.2-nts-vc15-x64.zip',
+          '5.4' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.4-nts-vc9-x64.zip',
+          '5.5' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.5-nts-vc11-x64.zip',
+          '5.6' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-5.6-nts-vc11-x64.zip',
+          '7.0' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.0-nts-vc14-x64.zip',
+          '7.1' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.1-nts-vc14-x64.zip',
+          '7.2' => 'http://windows.php.net/downloads/pecl/releases/varnish/1.2.4/php_varnish-1.2.4-7.2-nts-vc15-x64.zip',
         ),
       ),
     ),
