b4aa68dccf099c72cbd91f8e97765529fd7499b3
jakoch <jakoch@web.de>
2018-09-04 19:53:45 +0200

    updated software registry - Gimp v2.10.6

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Tue, 04 Sep 2018 17:53:40 +0000.
+    * Last Update Tue, 04 Sep 2018 17:53:43 +0000.
     * Do not edit manually!
     */
 
@@ -576,9 +576,10 @@
     '2.10.0' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.0-setup.exe',
     '2.10.2' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.2-setup.exe',
     '2.10.4' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.4-setup.exe',
+    '2.10.6' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.6-setup.exe',
     'latest' => array(
-      'version' => '2.10.4',
-      'url' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.4-setup.exe',
+      'version' => '2.10.6',
+      'url' => 'https://download.gimp.org/pub/gimp/stable/windows/gimp-2.10.6-setup.exe',
     ),
   ),
   'gogs-x64' => array(
