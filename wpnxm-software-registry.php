d167c8b8a10295ee29d23dde39064092325473d5
jakoch <jakoch@web.de>
2018-10-03 23:23:03 +0200

    updated software registry - FileZilla FTP Client x64 v3.37.3

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Wed, 03 Oct 2018 21:22:55 +0000.
+    * Last Update Wed, 03 Oct 2018 21:23:02 +0000.
     * Do not edit manually!
     */
 
@@ -504,9 +504,10 @@
     '3.36.0' => 'https://download.filezilla-project.org/client/FileZilla_3.36.0_win64.zip',
     '3.37.0' => 'https://download.filezilla-project.org/client/FileZilla_3.37.0_win64.zip',
     '3.37.1' => 'https://download.filezilla-project.org/client/FileZilla_3.37.1_win64.zip',
+    '3.37.3' => 'https://download.filezilla-project.org/client/FileZilla_3.37.3_win64.zip',
     'latest' => array(
-      'version' => '3.37.1',
-      'url' => 'https://download.filezilla-project.org/client/FileZilla_3.37.1_win64.zip',
+      'version' => '3.37.3',
+      'url' => 'https://download.filezilla-project.org/client/FileZilla_3.37.3_win64.zip',
     ),
   ),
   'filezilla-x86' => array(
