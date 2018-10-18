91766cc65faee9281d13a4f1fedb54ab95e0ef21
jakoch <jakoch@web.de>
2018-10-18 13:52:01 +0200

    updated software registry - ChromeDriver v2.43

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 08 Oct 2018 13:39:52 +0000.
+    * Last Update Thu, 18 Oct 2018 11:51:59 +0000.
     * Do not edit manually!
     */
 
@@ -261,9 +261,10 @@
     '2.40' => 'http://chromedriver.storage.googleapis.com/2.40/chromedriver_win32.zip',
     '2.41' => 'http://chromedriver.storage.googleapis.com/2.41/chromedriver_win32.zip',
     '2.42' => 'http://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip',
+    '2.43' => 'http://chromedriver.storage.googleapis.com/2.43/chromedriver_win32.zip',
     'latest' => array(
-      'version' => '2.42',
-      'url' => 'http://chromedriver.storage.googleapis.com/2.42/chromedriver_win32.zip',
+      'version' => '2.43',
+      'url' => 'http://chromedriver.storage.googleapis.com/2.43/chromedriver_win32.zip',
     ),
   ),
   'closure-compiler' => array(
