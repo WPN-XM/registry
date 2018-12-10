85c7f4cac8920ab15ba1d6e82aa4568dbfaab2fd
jakoch <jakoch@web.de>
2018-12-10 14:06:10 +0100

    updated software registry - OpenSSL (64-bit) v1.0.2q

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 10 Dec 2018 13:06:05 +0000.
+    * Last Update Mon, 10 Dec 2018 13:06:08 +0000.
     * Do not edit manually!
     */
 
@@ -2080,9 +2080,10 @@
     '1.0.2n' => 'http://indy.fulgan.com/SSL/openssl-1.0.2n-x64_86-win64.zip',
     '1.0.2o' => 'http://indy.fulgan.com/SSL/openssl-1.0.2o-x64_86-win64.zip',
     '1.0.2p' => 'http://indy.fulgan.com/SSL/openssl-1.0.2p-x64_86-win64.zip',
+    '1.0.2q' => 'http://indy.fulgan.com/SSL/openssl-1.0.2q-x64_86-win64.zip',
     'latest' => array(
-      'version' => '1.0.2p',
-      'url' => 'http://indy.fulgan.com/SSL/openssl-1.0.2p-x64_86-win64.zip',
+      'version' => '1.0.2q',
+      'url' => 'http://indy.fulgan.com/SSL/openssl-1.0.2q-x64_86-win64.zip',
     ),
   ),
   'orientdb' => array(
