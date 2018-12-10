0dcb15d639987164215d1f5fd61c3fc3032b87a8
jakoch <jakoch@web.de>
2018-12-10 14:05:01 +0100

    updated software registry - PHP QA x64 v7.3.0RC6

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 10 Dec 2018 13:04:56 +0000.
+    * Last Update Mon, 10 Dec 2018 13:04:59 +0000.
     * Do not edit manually!
     */
 
@@ -2523,10 +2523,11 @@
     '7.3.0beta3' => 'http://windows.php.net/downloads/qa/archives/php-7.3.0beta3-nts-Win32-VC15-x64.zip',
     '7.3.0RC1' => 'http://windows.php.net/downloads/qa/archives/php-7.3.0RC1-nts-Win32-VC15-x64.zip',
     '7.3.0RC2' => 'http://windows.php.net/downloads/qa/archives/php-7.3.0RC2-nts-Win32-VC15-x64.zip',
-    '7.3.0RC3' => 'http://windows.php.net/downloads/qa/php-7.3.0RC3-nts-Win32-VC15-x64.zip',
+    '7.3.0RC3' => 'http://windows.php.net/downloads/qa/archives/php-7.3.0RC3-nts-Win32-VC15-x64.zip',
+    '7.3.0RC6' => 'http://windows.php.net/downloads/qa/php-7.3.0RC6-nts-Win32-VC15-x64.zip',
     'latest' => array(
-      'version' => '7.3.0RC3',
-      'url' => 'http://windows.php.net/downloads/qa/php-7.3.0RC3-nts-Win32-VC15-x64.zip',
+      'version' => '7.3.0RC6',
+      'url' => 'http://windows.php.net/downloads/qa/php-7.3.0RC6-nts-Win32-VC15-x64.zip',
     ),
   ),
   'php-x64' => array(
