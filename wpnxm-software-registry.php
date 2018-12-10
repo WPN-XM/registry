e86fa0f262deb86fe3917aad37140f805d0c8332
jakoch <jakoch@web.de>
2018-12-10 14:06:07 +0100

    updated software registry - OpenSSL (32-bit) v1.0.2q

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Mon, 10 Dec 2018 13:06:01 +0000.
+    * Last Update Mon, 10 Dec 2018 13:06:05 +0000.
     * Do not edit manually!
     */
 
@@ -2019,9 +2019,10 @@
     '1.0.2n' => 'http://indy.fulgan.com/SSL/openssl-1.0.2n-i386-win32.zip',
     '1.0.2o' => 'http://indy.fulgan.com/SSL/openssl-1.0.2o-i386-win32.zip',
     '1.0.2p' => 'http://indy.fulgan.com/SSL/openssl-1.0.2p-i386-win32.zip',
+    '1.0.2q' => 'http://indy.fulgan.com/SSL/openssl-1.0.2q-i386-win32.zip',
     'latest' => array(
-      'version' => '1.0.2p',
-      'url' => 'http://indy.fulgan.com/SSL/openssl-1.0.2p-i386-win32.zip',
+      'version' => '1.0.2q',
+      'url' => 'http://indy.fulgan.com/SSL/openssl-1.0.2q-i386-win32.zip',
     ),
   ),
   'openssl-x64' => array(
