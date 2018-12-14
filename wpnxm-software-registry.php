9f3f272ffca2f1ceac8ff5984cc21690cfaa20bf
jakoch <jakoch@web.de>
2018-12-14 22:36:07 +0100

    updated software registry - Notepad++ v7.6.1

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 14 Dec 2018 21:35:58 +0000.
+    * Last Update Fri, 14 Dec 2018 21:36:05 +0000.
     * Do not edit manually!
     */
 
@@ -1953,9 +1953,10 @@
     '7.5.7' => 'https://notepad-plus-plus.org/repository/7.x/7.5.7/npp.7.5.7.bin.x64.zip',
     '7.5.8' => 'https://notepad-plus-plus.org/repository/7.x/7.5.8/npp.7.5.8.bin.x64.zip',
     '7.5.9' => 'https://notepad-plus-plus.org/repository/7.x/7.5.9/npp.7.5.9.bin.x64.zip',
+    '7.6.1' => 'https://notepad-plus-plus.org/repository/7.x/7.6.1/npp.7.6.1.bin.x64.zip',
     'latest' => array(
-      'version' => '7.5.9',
-      'url' => 'https://notepad-plus-plus.org/repository/7.x/7.5.9/npp.7.5.9.bin.x64.zip',
+      'version' => '7.6.1',
+      'url' => 'https://notepad-plus-plus.org/repository/7.x/7.6.1/npp.7.6.1.bin.x64.zip',
     ),
   ),
   'notepadplusplus-x86' => array(
