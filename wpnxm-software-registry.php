985bc1fa731a82bade1fccef706ffe01463c1a72
jakoch <jakoch@web.de>
2019-01-04 12:04:20 +0100

    updated software registry - NodeJS x64 v11.6.0

--- wpnxm-software-registry.php
+++ wpnxm-software-registry.php
@@ -11,7 +11,7 @@
    /**
     * WPИ-XM Server Stack - Software Registry
     * ---------------------------------------
-    * Last Update Fri, 04 Jan 2019 11:04:11 +0000.
+    * Last Update Fri, 04 Jan 2019 11:04:19 +0000.
     * Do not edit manually!
     */
 
@@ -1825,9 +1825,10 @@
     '11.3.0' => 'http://nodejs.org/dist/v11.3.0/win-x64/node.exe',
     '11.4.0' => 'http://nodejs.org/dist/v11.4.0/win-x64/node.exe',
     '11.5.0' => 'http://nodejs.org/dist/v11.5.0/win-x64/node.exe',
+    '11.6.0' => 'http://nodejs.org/dist/v11.6.0/win-x64/node.exe',
     'latest' => array(
-      'version' => '11.5.0',
-      'url' => 'http://nodejs.org/dist/v11.5.0/win-x64/node.exe',
+      'version' => '11.6.0',
+      'url' => 'http://nodejs.org/dist/v11.6.0/win-x64/node.exe',
     ),
   ),
   'nodenpm' => array(
